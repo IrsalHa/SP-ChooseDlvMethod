e48e943e1aa28d084e13126b7b3c7717dfaab29d
Irsal Hakim Alamsyah <45132491+IrsalHa@users.noreply.github.com>
2026-04-17 20:27:07 +0700

    Create f_closest_store_priority.sql

---
+++ f_closest_store_priority.sql
@@ -0,0 +1,34 @@
+CREATE OR REPLACE FUNCTION f_closest_store_priority(
+    in_province_id VARCHAR(30)
+)
+RETURNS TABLE (
+    site_stock VARCHAR(4),
+    site_name VARCHAR(50),
+    latitude DOUBLE PRECISION,
+    longitude DOUBLE PRECISION,
+    store_stock VARCHAR(3),
+    postal_code VARCHAR(10)
+) AS $$
+BEGIN
+    RETURN QUERY
+    SELECT 
+        sdspcp.delivery_site AS site_stock, 
+        hs.site_name, 
+        hs.latitude::DOUBLE PRECISION, 
+        hs.longitude::DOUBLE PRECISION, 
+        hs.sales_office_id AS store_stock, 
+        hs.postal_code
+    FROM 
+        s_delivery_site_priority_courier_3rd_party sdspcp
+    JOIN 
+        ft_s_hms_site hs 
+        ON sdspcp.delivery_site = hs.site_id
+        AND hs.is_setup = true
+        AND hs.status = true
+        AND hs.is_pickup_point = true
+    WHERE 
+        sdspcp.province_id = in_province_id
+    ORDER BY 
+        sdspcp.priority ASC;
+END;
+$$ LANGUAGE plpgsql;
